010aa74c955a3bae1a1bb68782fa260ea510f1b8
Gedalya Krycer <gedalya@krycer.com>
2020-11-12 13:23:21 -0800

    Set up base global styles

--- screenit/src/App.js
+++ screenit/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <h1>Screen it</h1>
+      <h2>Death Becomes Her</h2>
+      <h3>Comedy</h3>
+      <p>Name of the movie</p>
     </div>
   );
 }
