b15b111b89b21d9c6e70aec46ec4b3f9cb7c39c0
Gedalya Krycer <gedalya@krycer.com>
2020-11-12 15:33:31 -0800

    Form style updates

--- screenit/src/components/Filter/Filter.js
+++ screenit/src/components/Filter/Filter.js
@@ -5,44 +5,55 @@ const Filter = () => {
     return (
         <section className="filter">
             <form onSubmit={"handleSubmit"}>
+
                 {/* Name of movie */}
                 <div className="filter__input-group">
-                    <label className="filter__label">
-                        Name
-                        <input
-                            className="filter__input"
-                            type="text"
-                            placeholder="Name of the movie"
-                        />
-                    </label>
+                    <label
+                        for="nameInput"
+                        className="filter__label">Name</label>
+
+                    <input
+                        id="nameInput"
+                        type="text"
+                        placeholder="Name of the movie"
+                    />
                 </div>
 
                 {/* Category */}
                 <div className="filter__input-group">
-                    <label className="filter__label">Category
-                        <select value={"this.state.value"} onChange={"handleChange"}>
-                            <option value={null}>Select a category</option>
-                            <option value="action">Action</option>
-                            <option value="comedy">Comedy</option>
-                            <option value="drama">Drama</option>
-                            <option value="romance">Romance</option>
-                            <option value="horror">Horror</option>
-                            <option value="thriller">Thriller</option>
-                            <option value="documentary">Documentary</option>
-                            <option value="other">Other</option>
-                        </select>
-                    </label>
+                    <label
+                        className="filter__label"
+                    >Category</label>
+                    <select
+                        value={"this.state.value"}
+                        onChange={"handleChange"}>
+                        <option value={null}>Select a category</option>
+                        <option value="action">Action</option>
+                        <option value="comedy">Comedy</option>
+                        <option value="drama">Drama</option>
+                        <option value="romance">Romance</option>
+                        <option value="horror">Horror</option>
+                        <option value="thriller">Thriller</option>
+                        <option value="documentary">Documentary</option>
+                        <option value="other">Other</option>
+                    </select>
+
                 </div>
 
                 {/* Category */}
-                <div className="filter__input-group">
-                    <label className="filter__label">Rating
-                    More to come
-                    </label>
+                <div className="filter__input-group--rating">
+                    <label className="filter__label">Rating</label>
+                    Stars
                 </div>
 
-                <input type="submit" value="submit" />
+                {/* Submit Button */}
+                <input
+                    className="filter__submit-btn"
+                    type="submit"
+                    value="Add Movie"
+                />
             </form>
+
         </section>
     )
 }
