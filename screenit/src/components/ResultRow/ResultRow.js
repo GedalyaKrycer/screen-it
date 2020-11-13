58e2a97e9b875fe51e28400b0dbda3aa8da56e19
Gedalya Krycer <gedalya@krycer.com>
2020-11-12 21:54:45 -0800

    Limited avatar to 3 characters

--- screenit/src/components/ResultRow/ResultRow.js
+++ screenit/src/components/ResultRow/ResultRow.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import RatingStarsDisplay from '../RatingStarsDisplay/RatingStarsDisplay';
 import { useSeenItContext } from '../../utils/SeenItContext';
@@ -7,14 +7,32 @@ const ResultRow = () => {
     // Imports states from context
     const { resultsArray } = useSeenItContext();
 
+
+
+
+
     return (
         <>
             {
                 resultsArray.map((result, index) => {
+
+                    // Take the movie's name and creates a new array, splitting on the spaces, but stops after 3 items.
+                    const avatarArray = result.movieName.split(" ").slice(0, 3)
+
+                    const avatarCharacters = [];
+
+                    avatarArray.forEach(e => {
+                        avatarCharacters.push(e.charAt(0));
+                    })
+
+
+
+                    console.log(avatarCharacters);
+
                     return <div
                         key={result.movieName + index}
                         className="result">
-                        <span className="result__avatar">Dbh</span>
+                        <span className="result__avatar">{avatarCharacters}</span>
 
                         <div>
                             <h2 className="result__movie-name">{result.movieName.trim('')}</h2>
