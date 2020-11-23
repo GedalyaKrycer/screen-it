93c954af120fa8742869dccc9b6c4b583c4e86ba
Gedalya Krycer <gedalya@krycer.com>
2020-11-22 19:50:28 -0800

    Added Clear All Button in Header

--- screenit/src/components/Header/Header.js
+++ screenit/src/components/Header/Header.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './style.css';
+import { useSeenItContext } from '../../utils/SeenItContext';
 
 const Header = () => {
+
+    // Imports states from context
+    const {
+        resultsArray,
+        setResultsArray
+    } = useSeenItContext();
+
+
+    // className={resultsArray.length >= 1 ? "header__btn--active" : "header__btn--disabled"}
     return (
         <header>
             <h1>Screen it</h1>
+            <button
+                className={`header__btn ${resultsArray.length >= 1 ? "header__btn--active" : "header__btn--disabled"}`}
+                disabled={resultsArray.length >= 1 ? false : true}
+                onClick={() => setResultsArray([])}
+            >Clear All</button>
         </header>
     )
 }
